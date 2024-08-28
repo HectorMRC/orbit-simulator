a3cf515fe4e81909d5c5257dcd7c26e998e9087f
hectormrc <thehector1593@gmail.com>
2024-08-28 22:59:03 +0200

    feat: add orbital system struct

--- src/geographic/orbit.rs
+++ src/geographic/orbit.rs
@@ -3,8 +3,11 @@ use crate::{cartesian::shape::Arc, Distance, Frequency, Mass, Velocity};
 /// The gravitational constant as N⋅m^2⋅kg^−2.
 pub const G: f64 = 6.67430e-11;
 
+/// The orbit of an object around a central body.
 pub trait Orbit {
+    /// The orbital velocity of the object.
     fn velocity(&self, central_body: Body) -> Velocity;
+    /// The orbit's frequency.
     fn frequency(&self, central_body: Body) -> Frequency;
 }
 
@@ -15,7 +18,7 @@ impl Orbit for Arc {
     }
 
     fn frequency(&self, central_body: Body) -> Frequency {
-        Frequency::hz(self.perimeter().as_meters() / self.velocity(central_body).as_meters_sec())
+        Frequency::hz(self.velocity(central_body).as_meters_sec() / self.perimeter().as_meters())
     }
 }
 
@@ -25,4 +28,15 @@ pub struct Body {
     pub radius: Distance,
     /// The mass of the body.
     pub mass: Mass,
+}
+
+/// An orbital system.
+pub struct System {
+    /// The central body of the system.
+    pub primary: Body,
+    /// The distance between the surface of the primary body of this system and that of the one it
+    /// orbits, if any.
+    pub radius: Option<Distance>,
+    /// The systems orbiting the primary body.
+    pub secondary: Vec<System>,
 }
