18334fc6e9cf960f58b951d1e0655a4b57b70f44
hectormrc <thehector1593@gmail.com>
2024-09-01 15:44:40 +0200

    fix: add missing name on bodies

--- globe-render/src/main.rs
+++ globe-render/src/main.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
+
 use bevy::prelude::*;
 use globe_render::{Globe2DPlugin, Storage};
 use globe_rs::{Body, Distance, Frequency, Mass};
+use alvidir::name::Name;
 
 fn main() {
     App::new()
@@ -13,7 +16,7 @@ fn main() {
 fn load_storage() -> Storage {
     let system = globe_rs::System {
         primary: Body {
-            // the sun
+            name: Name::from_str("Sun").unwrap(),
             radius: Distance::km(696_340.),
             rotation: Frequency::hz(4.2866941e-7),
             mass: Mass::kg(1.9891e30),
@@ -21,7 +24,7 @@ fn load_storage() -> Storage {
         distance: Distance::NONE,
         secondary: vec![globe_rs::System {
             primary: Body {
-                // the earth
+                name: Name::from_str("Earth").unwrap(),
                 radius: Distance::km(6_371.),
                 rotation: Frequency::hz(1.1574074e-5),
                 mass: Mass::kg(5.97219e24),
@@ -29,7 +32,7 @@ fn load_storage() -> Storage {
             distance: Distance::km(151_032_912.),
             secondary: vec![globe_rs::System {
                 primary: Body {
-                    // the moon
+                    name: Name::from_str("Moon").unwrap(),
                     radius: Distance::km(1_737.4),
                     rotation: Frequency::hz(4.2364839e-7),
                     mass: Mass::kg(7.34767309e22),
